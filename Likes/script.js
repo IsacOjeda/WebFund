e4ca14c90f41f84dbf289dbbcd1e58b9f3ece502
Isac <isacojeda717@gmail.com>
2023-10-16 17:05:05 -0700

    LikesAssignment

---
+++ Likes/script.js
@@ -0,0 +1,26 @@
+var liketext1 = document.querySelector("#count1")
+var count1 = 9
+function likes1(){
+    count1++;
+    console.log(count1)
+    liketext1.innerText = count1+ " like(s)";
+
+}
+
+var liketext2 = document.querySelector("#count2")
+var count2 = 12
+function likes2(){
+    count2++;
+    console.log(count2)
+    liketext2.innerText = count2+ " like(s)";
+
+}
+
+var liketext3 = document.querySelector("#count3")
+var count3 = 9
+function likes3(){
+    count3++;
+    console.log(count3)
+    liketext3.innerText = count3+ " like(s)";
+
+}
